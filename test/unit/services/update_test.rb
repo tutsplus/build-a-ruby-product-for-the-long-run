8695a9714dbf10399907851c8424d26bafbfb972
José Mota <jose@josemota.net>
2013-12-05 16:50:13 +0000

    Update id of bookmark to test.

--- test/unit/services/update_test.rb
+++ test/unit/services/update_test.rb
@@ -7,7 +7,7 @@ require "json"
 module Dainty::Services
   describe Update do
     it "creates a bookmark" do
-      id = 3
+      id = 2
       options = { title: "Jose Mota - Homepage", url: "http://www.josemota.net" }
       Update.new(id, options).update.must_equal 204
     end
