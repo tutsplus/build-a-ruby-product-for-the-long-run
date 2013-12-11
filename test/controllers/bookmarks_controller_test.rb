1f4377ddca390cc90c087d4e97f95b9f848cf98c
José Mota <jose@josemota.net>
2013-12-11 17:19:32 +0000

    Write test for bookmarks controller - create action.

--- test/controllers/bookmarks_controller_test.rb
+++ test/controllers/bookmarks_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BookmarksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "return ok status if bookmark is created" do
+    post :create, bookmark: { title: "Tuts+", url: "http://tutsplus.com" }
+
+    assert_response 201
+  end
+
+  test "returns not ok status if bookmark is not created" do
+    post :create, bookmark: {}
+
+    assert_response 422
+  end
 end
