eed841f45ee2043394e1a66e30d3358e764f09ba
José Mota <jose@josemota.net>
2013-12-11 16:59:31 +0000

    Add validation for bookmarks.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,3 @@
 class Bookmark < ActiveRecord::Base
+  validates_presence_of :title, :url
 end
--- test/services/create_bookmark_test.rb
+++ test/services/create_bookmark_test.rb
@@ -5,4 +5,8 @@ describe CreateBookmark do
   it "creates a bookmark" do
     assert CreateBookmark.new({ title: "Tuts+", url: "http://tutsplus.com"}).create
   end
+
+  it "fails to create a bookmark if there's no data" do
+    refute CreateBookmark.new({}).create
+  end
 end
