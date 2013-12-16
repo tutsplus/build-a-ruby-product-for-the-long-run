edf745d14a05e029bbb88c05b26dfbd83b326fce
José Mota <jose@josemota.net>
2013-12-16 17:06:18 +0000

    Add docs for ListBookmarks service.

--- app/services/list_bookmarks.rb
+++ app/services/list_bookmarks.rb
@@ -1,4 +1,12 @@
+##
+# This class allows users to list all of their bookmarks. It consists of a
+# single method that retrieves an array of {Bookmark} objects.
+#
 class ListBookmarks
+  ##
+  # The main method of the service. Retrieves a list of bookmarks.
+  #
+  # @return Array
   def self.list
     Bookmark.all.to_a
   end
